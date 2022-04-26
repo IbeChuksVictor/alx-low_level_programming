ff0f1e0ef4cbce94a2cb07e7067e7fbeef0a4bda
Ibegbulem Chukwuma Victor <ibechuksvictor@gmail.com>
2022-04-26 00:54:00 -0700

    update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,39 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include "lists.h"
 
 /**
-* print_listint_safe - a function that prints a listint_t
-*linked list
-* @head: points to the beginning of a linked list
-* Return: the number of nodes in the list
-**/
+ * detect_loop - Detect infinite loop in linked list.
+ * @head: head node of linked list
+ * Return: number of nodes in the list if a loop is detected, otherwise 0
+ */
+size_t detect_loop(const listint_t *head)
+{
+	const listint_t *slow, *fast;
+	size_t i;
 
+	i = 1;
+	slow = fast = head;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		++i;
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow->next != fast->next)
+			{
+				slow = slow->next;
+				fast = fast->next;
+				++i;
+			}
+			return (i);
+		}
+	}
+	return (0);
+}
+
+
+
+/**
+ * print_listint_safe - Print a listint_t linked list.
+ * @head: head node of the linked list
+ * Return: number of nodes in the linked list, otherwise
+ *         returns a status code 98
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *f_ptr, *s_ptr;
-	size_t size;
-
-	size = 0;
+	size_t i, number_of_nodes;
 
 	if (head == NULL)
-		return (0);
+		return (98);
 
-	s_ptr = head;
-	f_ptr = head->next;
-
-	while (f_ptr != NULL && f_ptr < s_ptr)
+	number_of_nodes = detect_loop(head);
+	if (number_of_nodes)
 	{
-		size += 1;
-	printf("[%p] %i\n", (void *)s_ptr, s_ptr->n);
-		s_ptr = s_ptr->next;
-		f_ptr = f_ptr->next;
+		for (i = 0; i < number_of_nodes; ++i)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		printf("-> [%p] %d\n", (void *)head, head->n);
+		return (number_of_nodes);
 	}
-	printf("[%p] %i\n", (void *)s_ptr, s_ptr->n);
-	size += 1;
-	if (f_ptr)
-		printf("-> [%p] %i\n", (void *)f_ptr, f_ptr->n);
-
-	return (size);
+	for (i = 0; head; ++i)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+	}
+	return (i);
 }
